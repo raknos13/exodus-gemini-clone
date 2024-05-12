a5be06985bf0805f20640d6dca46d0197fc924b1
Mukesh Kumar <mkso13189@gmail.com>
2024-05-12 17:32:33 +0530

    added CSS styling to sidebar components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,10 +4,10 @@ import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <div className='top'>
-        <img className='menu' src={assets.menu_icon} alt="" />
-        <div className='new-chat'>
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
           <p>New Chat</p>
         </div>
@@ -18,19 +18,19 @@ const Sidebar = () => {
             <p>What is react ...</p>
           </div>
         </div>
-        <div className="bottom">
-          <div className="bottom-item recent-entry">
-            <img src={assets.question_icon} alt="" />
-            <p>Help</p>
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.history_icon} alt="" />
-            <p>Activity</p>
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.setting_icon} alt="" />
-            <p>Settings</p>
-          </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Settings</p>
         </div>
       </div>
     </div>
